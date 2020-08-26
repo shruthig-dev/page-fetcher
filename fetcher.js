64ba0eac18b4d76beafa7aaaa586eda50450c3a8
vagrant <shruthigangadharblr@gmail.com>
2020-08-26 17:49:39 +0000

    request.js issue fixed

--- fetcher.js
+++ fetcher.js
@@ -1,4 +1,4 @@
-const request = require('axios');
+const request = require('request');
 const fs = require('fs');
 const readline = require('readline');
 
@@ -10,47 +10,51 @@ readline.createInterface({
   output: process.stdout
 });
 
-request.get(url)
-  .then(function(response) {
-    
-    if (fs.existsSync(path)) {
-      console.log("\nFile already exists. Please enter y/Y to overwrite the file or n/N to skip and exit");
-      
-      process.stdin.on('data', (key) => {
+if (url && path) {
+  request(url,(error, response, body) => {
 
-        if (String(key) === "y" || String(key) === "Y") {
-          fs.writeFile(path, response.data, function(err) {
-            if (err) {
-              return console.log(err);
-            }
-            let fileSize = (response !== null && response.data !== null) ? response.data.length : 0;
-            console.log(`\nDownloaded and saved ${fileSize} bytes to ${path}`);
-            console.log('---------------------------------------------------------------------');
-            process.exit();
-          });
-        }
-        if (String(key) === "n" || String(key) === "N") {
-          console.log("\nDownload Cancelled !.");
-          process.exit();
-        } else if (String(key) === '\u0003') {
-          process.exit();
-        } else {
-          console.log("\nPlease enter valid input");
-        }
-      });
-    } else {
-      console.log("\nInvalid file path or file path not found. Please enter valid file path.");
+    if (error) {
+      console.log(`Invalid URL.Please enter valid URL `);
       console.log('---------------------------------------------------------------------');
+      console.log(`Please see the errors  \n ${error}`);
       process.exit();
+    } else {
+
+      if (fs.existsSync(path)) {
+
+        console.log("\nFile already exists. Please enter y/Y to overwrite the file or n/N to skip and exit");
+        
+        process.stdin.on('data', (key) => {
+
+          if (String(key) === "y" || String(key) === "Y") {
+            fs.writeFile(path, body, function(err) {
+              if (err) {
+                return console.log(err);
+              }
+              let fileSize = (body !== null) ? body.length : 0;
+              console.log(`\nDownloaded and saved ${fileSize} bytes to ${path}`);
+              console.log('---------------------------------------------------------------------');
+              process.exit();
+            });
+          } else if (String(key) === "n" || String(key) === "N") {
+            console.log("\nDownload Cancelled !.");
+            process.exit();
+          } else if (String(key) === '\u0003') {
+            process.exit();
+          } else {
+            console.log("\nPlease enter valid input");
+          }
+        });
+      } else {
+        console.log("\nInvalid file path or file path not found. Please enter valid file path.");
+        console.log('---------------------------------------------------------------------');
+        process.exit();
+      }
     }
-  })
-  .catch(function(error) {
-    console.log(`Invalid URL.Please enter valid URL `);
-    console.log('---------------------------------------------------------------------');
-    console.log(`Please see below the error  : ${error}`);
-    process.exit();
-  })
-  .then(function() {
-    // always executed
-    
   });
+} else {
+  console.log("Please enter input values (Url & file path) ");
+  process.exit();
+}
+
+//node fetcher.js http://www.example.edu/ ./index.html
